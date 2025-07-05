c8868d5909bfdf0306a4e55f75f66a295b7eec2b
Pisnut <rayner.freddie@btinternet.com>
2025-07-05 08:24:10 +0100

    Update Homepage for mobile screens

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,11 +4,11 @@ import { faEnvelope } from "@fortawesome/free-solid-svg-icons"
 import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 export default function Footer() {
-    return <nav id="Footer">
+    return <div id="Footer">
         <ul id="Footer-list">
             <li><a href="https://github.com/Jefernater58"><FontAwesomeIcon className="Footer-icon" icon={faGithub} size="2x"/></a></li>
             <li><a href="https://www.linkedin.com/in/freddie-rayner/"><FontAwesomeIcon className="Footer-icon" icon={faLinkedin} size="2x"/></a></li>
             <li><a href="mailto:freddie@fwrayner.uk"><FontAwesomeIcon className="Footer-icon" icon={faEnvelope} size="2x"/></a></li>
         </ul>
-    </nav>;
+    </div>;
 }
