3b7cdb871c575c66d271b587b1574deb5b6587bc
Pisnut <rayner.freddie@btinternet.com>
2025-07-15 17:46:19 +0100

    Started Projects page

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -6,7 +6,9 @@ export default function Home() {
     return <div className="Container">
         <Navbar/>
         <div className="Projects">
-            <h2>This page is under construction</h2>
+            <div className="Projects-project"></div>
+            <div className="Projects-project"></div>
+            <div className="Projects-project"></div>
         </div>
         <Footer/>
     </div>;
